4e50ead0b7f25af5d42e3495f9dc41cc6504f488
Miles Pratt <miles@thoughtpowder.com>
2020-06-06 10:41:31 -0400

    remove config for cname. add path prefixing.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@
 
 module.exports = {
   /* Your site config here */
-  pathPrefix: "/gatsby-md-poc",
+  pathPrefix: "/Police-Data-Accessibility-Project.github.io",
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
